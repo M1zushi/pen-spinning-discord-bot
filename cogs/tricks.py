ad38cc16bae64562228d50fdb0f3f0f02ea38c7c
GhostMander <58628835+GhostMander@users.noreply.github.com>
2021-07-03 21:19:05 +0530

    Formatting

--- cogs/tricks.py
+++ cogs/tricks.py
@@ -1,11 +1,5 @@
-import discord
 from discord.ext import commands
 
-import asyncio
-import random
-
-import os
-import sys
 
 class Tricks(commands.Cog):
 
@@ -13,95 +7,94 @@ class Tricks(commands.Cog):
         self.client = client
 
     # Fundementals
-    @commands.command(aliases = ['ta'])
-    async def thumbaround(self, ctx, mod='none'):
+    @commands.command(name="Thumbaround", aliases=['ta'])
+    async def _thumbaround(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Thumbaround Tutorial: \nhttps://youtu.be/vEvPPVCH6cg')
+            await ctx.send('__**Thumbaround Tutorial:**__ https://youtu.be/vEvPPVCH6cg')
         elif 'rev' in mod.lower():
-            await ctx.send('Thumbaround Reverse Tutorial: \nhttps://youtu.be/gAf0BbipZ-I')
+            await ctx.send('__**Thumbaround Reverse Tutorial:**__ https://youtu.be/gAf0BbipZ-I')
 
-    @commands.command(aliases = ['fp'])
-    async def fingerpass(self, ctx, mod='none'):
+    @commands.command(name="Fingerpass", aliases=['fp'])
+    async def _fingerpass(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Fingerpass Tutorial: \nhttps://youtu.be/0EuJwfJHTyI')
+            await ctx.send('__**Fingerpass Tutorial:**__ https://youtu.be/0EuJwfJHTyI')
         elif 'rev' in mod.lower():
-            await ctx.send('Fingerpass Reverse Tutorial: \nhttps://youtu.be/-BLoyJtbpn8')
+            await ctx.send('__**Fingerpass Reverse Tutorial:**__ https://youtu.be/-BLoyJtbpn8')
 
-    @commands.command(aliases = [''])
-    async def sonic(self, ctx, mod='none'):
+    @commands.command(name="Sonic", aliases=[])
+    async def _sonic(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Sonic Tutorial: \nhttps://youtu.be/ueeZ3W7DIQo')
+            await ctx.send('__**Sonic Tutorial:**__ https://youtu.be/ueeZ3W7DIQo')
         elif 'rev' in mod.lower():
-            await ctx.send('Sonic Reverse Tutorial: \nhttps://youtu.be/5nh_cXf71l8')
+            await ctx.send('__**Sonic Reverse Tutorial:**__ https://youtu.be/5nh_cXf71l8')
 
-    @commands.command(aliases = [''])
-    async def charge(self, ctx, mod='none'):
+    @commands.command(name="Charge", aliases=[])
+    async def _charge(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Charge Tutorial: \nhttps://youtu.be/AIe1jCYFNw0')
+            await ctx.send('__**Charge Tutorial:**__ https://youtu.be/AIe1jCYFNw0')
         elif 'rev' in mod.lower():
-            await ctx.send('Charge Reverse Tutorial: \nhttps://youtu.be/kGyShNYrPhs')
-
+            await ctx.send('__**Charge Reverse Tutorial:**__ https://youtu.be/kGyShNYrPhs')
 
     # Mega's Basics
-    @commands.command(aliases = ['tws', 'twisted sonic', 'twistedsonic'])
-    async def twisted_sonic(self, ctx, mod='none'):
-        if mod == 'none':
-            await ctx.send('Twisted Sonic Tutorial: \nhttps://youtu.be/lCPB05Hifhw')
 
-    @commands.command(aliases = ['bak'])
-    async def backaround(self, ctx, mod='none'):
+    @commands.command(name="Twisted Sonic", aliases=['tws', 'twisted_sonic', 'twistedsonic'])
+    async def _twisted_sonic(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Backaround Tutorial: \nhttps://youtu.be/DaOCyWGZRCM')
+            await ctx.send('__**Twisted Sonic Tutorial:**__ https://youtu.be/lCPB05Hifhw')
 
-    @commands.command(aliases = ['scizzor spin', 'scizzorspin'])
-    async def scizzor_spin(self, ctx, mod='none'):
+    @commands.command(name="Backaround", aliases=['bak'])
+    async def _backaround(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Scizzor Spin Tutorial: \nhttps://youtu.be/PUTTwcuzC1U')
+            await ctx.send('__**Backaround Tutorial:**__ https://youtu.be/DaOCyWGZRCM')
 
-    @commands.command(aliases = ['neo sonic'])
-    async def neosonic(self, ctx, mod='none'):
+    @commands.command(name="Scizzor Spin", aliases=['scizzor_spin', 'scizzorspin'])
+    async def _scizzor_spin(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Neosonic: \nhttps://youtu.be/GnMmulpAMM8')
+            await ctx.send('__**Scizzor Spin Tutorial:**__ https://youtu.be/PUTTwcuzC1U')
+
+    @commands.command(name="Neosonic", aliases=['neo sonic'])
+    async def _neosonic(self, ctx, mod='none'):
+        if mod == 'none':
+            await ctx.send('__**Neosonic:**__ https://youtu.be/GnMmulpAMM8')
 
     # Mega's Confirmed
-    @commands.command(aliases = ['inverse sonic', 'inv sonic'])
-    async def inverse_sonic(self, ctx, mod='none'):
+    @commands.command(name="Inverse Sonic", aliases=['inverse_sonic', 'inv sonic'])
+    async def _inverse_sonic(self, ctx, mod='none'):
         if mod == 'none':
-            await ctx.send('Inverse Sonic: \nhttps://youtu.be/k0RhL8a4V9g')
+            await ctx.send('__**Inverse Sonic:**__ https://youtu.be/k0RhL8a4V9g')
 
     # Mega's Advanced
-
 
     # Mega's Expert
 
 
-
 tricks = ['thumbaround',
-    'charge',
-    'sonic',
-    'fingerpass',
-    'thumbaround reverse'
-    'charge reverse',
-    'sonic reverse',
-    'fingerpass reverse',
-    'neosonic',
-    'neosonic reverse',
-    'inverse sonic',
-    'inverse sonic reverse',
-    'wiper',
-    'wiper reverse',
-    'twisted sonic',
-    'twisted sonic reverse',
-    'backaround',
-    'bust',
-    'indexaround',
-    'indexaround reverse',
-    'fingless thumaround',
-    'fingerless thumbaround reverse',
-    'scizzor spin',
-    'extended thumbaround',
-    'extended thumbaround reverse',
-    'neobackaround']
+          'charge',
+          'sonic',
+          'fingerpass',
+          'thumbaround reverse'
+          'charge reverse',
+          'sonic reverse',
+          'fingerpass reverse',
+          'neosonic',
+          'neosonic reverse',
+          'inverse sonic',
+          'inverse sonic reverse',
+          'wiper',
+          'wiper reverse',
+          'twisted sonic',
+          'twisted sonic reverse',
+          'backaround',
+          'bust',
+          'indexaround',
+          'indexaround reverse',
+          'fingless thumaround',
+          'fingerless thumbaround reverse',
+          'scizzor spin',
+          'extended thumbaround',
+          'extended thumbaround reverse',
+          'neobackaround']
+
 
 def setup(client):
     client.add_cog(Tricks(client))
--- spin.py
+++ spin.py
@@ -2,40 +2,119 @@
 # https://discord.gg/UEwf6RWqU2
 # Created by: Mizu (M1zushi)
 
+import os
+
 # PACKAGES & IMPORTS
 import discord
-
-import random
 from discord.ext import commands
-
-import os
 from dotenv import load_dotenv
+
 load_dotenv('.env')
 
 
+class EmbedHelpCommand(commands.HelpCommand):
+    # Set the embed colour here
+    COLOUR = discord.Colour(0xff6900)
+
+    def get_ending_note(self):
+        return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
+
+    def get_command_signature(self, command):
+        return '{0.qualified_name} {0.signature}'.format(command)
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title='Bot Commands', colour=self.COLOUR)
+        description = self.context.bot.description
+        if description:
+            embed.description = description
+        embed.set_author(name=client.user.name,
+                         icon_url=client.user.avatar_url)
+
+        for cog, commands in mapping.items():
+            name = '\u200b' if cog is None else cog.qualified_name
+            filtered = await self.filter_commands(commands, sort=True)
+            if filtered:
+                value = '\n'.join(
+                    f"{f'** {c.name} **'.ljust(20, ' ')} -\t{c.brief}" for c in commands)
+                if cog and cog.description:
+                    value = '{0}\n{1}'.format(cog.description, value)
+
+                embed.add_field(name=name, value=value)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    async def send_cog_help(self, cog):
+        embed = discord.Embed(
+            title='{0.qualified_name} Commands'.format(cog), colour=self.COLOUR)
+        embed.set_author(name=client.user.name,
+                         icon_url=client.user.avatar_url)
+
+        if cog.description:
+            embed.description = cog.description
+
+        filtered = await self.filter_commands(cog.get_commands(), sort=True)
+        for command in filtered:
+            embed.add_field(name=self.get_command_signature(
+                command), value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    async def send_group_help(self, group):
+        embed = discord.Embed(title=group.qualified_name, colour=self.COLOUR)
+        embed.set_author(name=client.user.name,
+                         icon_url=client.user.avatar_url)
+        if group.help:
+            embed.description = group.help
+
+        if isinstance(group, commands.Group):
+            filtered = await self.filter_commands(group.commands, sort=True)
+            for command in filtered:
+                embed.add_field(name=self.get_command_signature(
+                    command), value=command.short_doc or '...', inline=False)
+
+        embed.set_footer(text=self.get_ending_note())
+        await self.get_destination().send(embed=embed)
+
+    # This makes it so it uses the function above
+    # Less work for us to do since they're both similar.
+    # If you want to make regular command help look different then override it
+    send_command_help = send_group_help
+
+
 # SETS UP BASICS
-client = commands.Bot(command_prefix = 'ps ',
-                      case_insensitive = True,
-                      activity = discord.Activity(type=discord.ActivityType.playing, name='sls spam'),
-                      status = discord.Status.dnd
+client = commands.Bot(command_prefix=('ps ', 'ps'),
+                      case_insensitive=True,
+                      activity=discord.Activity(
+                          type=discord.ActivityType.playing, name='sls spam'),
+                      status=discord.Status.dnd,
+                      help_command=EmbedHelpCommand(),
                       )
-client.remove_command('help')
+
+
+# client.remove_command('help')
+
 
 @client.event
 async def on_ready():
     print('Time to spam sls.')
 
-@client.command()
-async def ping(ctx):
+
+@client.command(name="ping", brief="This is Brief", help="This is help.")
+async def _ping(ctx):
     await ctx.send(f'Pen is spinning {round(client.latency * 1000)}ms late.')
 
-@client.command()
-async def drop(ctx):
+
+@client.command(name="drop")
+async def _drop(ctx):
     if ctx.author.id in {int(os.getenv('MIZU_ID'))}:
+        await ctx.send('Yaamete~! Nooo! Not theree~!')
         await ctx.send('I dropped my mod...')
         await client.logout()
     else:
         await ctx.send('I only listen to Mizu-kun!')
+
 
 # HELP COMMAND: GIVES INFO ABOUT USING THE BOT
 # @client.command()
@@ -44,128 +123,153 @@ async def drop(ctx):
 
 
 # ATTACHES POSTS ABOUT LEARNING ORDER
-@client.command()
-async def trickorder(ctx):
+@client.command(name="Trick Order")
+async def _trickorder(ctx):
     await ctx.send(file=discord.File('pics/megatrickorder.png'))
 
-@client.command()
-async def isuktrickorder(ctx):
+
+@client.command(name="is uk trick order")
+async def _isuktrickorder(ctx):
     await ctx.send('http://isukps.blogspot.com/2017/01/power-trick-learning-and-difficulty.html')
 
 
 # INVITES TO DISCORD PS SERVERS
-@client.command()
-async def discord_workshop(ctx):
+@client.command(name="Discord Workshop")
+async def _discord_workshop(ctx):
     await ctx.send('https://discord.gg/aCtV5ZAu9X')
 
-@client.command()
-async def discord_reddit(ctx):
+
+@client.command(name="Discord Reddit")
+async def _discord_reddit(ctx):
     await ctx.send('https://discord.gg/9bVNK8mefb')
 
-@client.command()
-async def discord_penstock(ctx):
+
+@client.command(name="Discord PenStock")
+async def _discord_penstock(ctx):
     await ctx.send('https://discord.gg/BxfewGYbQM')
 
-@client.command()
-async def discord_penmodding(ctx):
+
+@client.command(name="Discord PenModding")
+async def _discord_penmodding(ctx):
     await ctx.send('https://discord.gg/vXVWZz6UCv')
 
-@client.command()
-async def discord_mango(ctx):
+
+@client.command(name="Discord Mango")
+async def _discord_mango(ctx):
     await ctx.send('https://discord.gg/Hrg6FHf')
 
-@client.command()
-async def discord_fenspinner(ctx):
+
+@client.command(name="Discord PenSpinner")
+async def _discord_fenspinner(ctx):
     await ctx.send('https://discord.gg/U3CpbYY3fP')
 
-@client.command()
-async def discord_frenchboard(ctx):
+
+@client.command(name="Discord FrenchBoard")
+async def _discord_frenchboard(ctx):
     await ctx.send('https://discord.gg/zSWBsd4c3b')
 
-@client.command()
-async def discord_thaispinner(ctx):
+
+@client.command(name="Discord ThaiSpinner")
+async def _discord_thaispinner(ctx):
     await ctx.send('https://discord.gg/QjGZh2t')
 
-@client.command()
-async def discord_polishboard(ctx):
+
+@client.command(name="Discord PolishBoard")
+async def _discord_polishboard(ctx):
     await ctx.send('https://discord.gg/MCXEnkh')
 
-@client.command()
-async def discord_germancommunity(ctx):
+
+@client.command(name="Discord GermanCommunity")
+async def _discord_germancommunity(ctx):
     await ctx.send('https://discord.com/invite/aUuz8sh')
 
-@client.command()
-async def discord_birthdaycrimes(ctx):
+
+@client.command(name="Discord BirthdayCrimes")
+async def _discord_birthdaycrimes(ctx):
     await ctx.send('https://discord.gg/mYkh4nu9Uv')
 
 
 # ATTACHES EXPLANATION FOR PS NOTATIONS
-@client.command()
-async def notation(ctx):
+@client.command(name="SLSn")
+async def _notation(ctx):
     await ctx.send('''```
 
-    ```''')
+```''')
 
 
 # ATTACHES LINKS FOR PS LINKAGES
-@client.command()
-async def breakdowngenerator(ctx):
+@client.command(name="Breakdown Generator")
+async def _breakdowngenerator(ctx):
     await ctx.send('https://limezzje.github.io/PS/')
 
-@client.command()
-async def sls(ctx):
+
+@client.command(name="SLS")
+async def _sls(ctx):
     await ctx.send('Playlist: https://youtube.com/playlist?list=PLqvo4PPYx7nU8j3Cjz2u6dZ7s0FERNJ7y')
 
-@client.command()
-async def sls1(ctx):
+
+@client.command(name="SLS1")
+async def _sls1(ctx):
     await ctx.send('SLS 1: https://youtu.be/a7AoLeswfuU')
 
-@client.command()
-async def sls2(ctx):
+
+@client.command(name="SLS2")
+async def _sls2(ctx):
     await ctx.send('SLS 2: https://youtu.be/S0XiczC333A')
 
-@client.command()
-async def sls3(ctx):
+
+@client.command(name="SLS3")
+async def _sls3(ctx):
     await ctx.send('SLS 3: https://youtu.be/7sSxBwdLsz0')
 
-@client.command()
-async def sls4(ctx):
+
+@client.command(name="SLS4")
+async def _sls4(ctx):
     await ctx.send('SLS 4: https://youtu.be/JAnskN6yrI4')
 
-@client.command()
-async def sls5(ctx):
+
+@client.command(name="SLS5")
+async def _sls5(ctx):
     await ctx.send('SLS 5: https://youtu.be/sTGL8CwVmUU')
 
-@client.command()
-async def sls6(ctx):
+
+@client.command(name="SLS6")
+async def _sls6(ctx):
     await ctx.send('SLS 6: https://youtu.be/O0pGMTkd1X8')
 
-@client.command()
-async def sls7(ctx):
+
+@client.command(name="SLS7")
+async def _sls7(ctx):
     await ctx.send('SLS 7: https://youtu.be/GL16ck0nB7w')
 
-@client.command()
-async def sls8(ctx):
+
+@client.command(name="SLS8")
+async def _sls8(ctx):
     await ctx.send('SLS 8: https://youtu.be/IIlHERIYS9s')
 
-@client.command()
-async def sls9(ctx):
+
+@client.command(name="SLS9")
+async def _sls9(ctx):
     await ctx.send('SLS 9: https://youtu.be/bNq79vmZLb0')
 
-@client.command()
-async def sls10(ctx):
+
+@client.command(name="SLS10")
+async def _sls10(ctx):
     await ctx.send('SLS 10: https://youtu.be/k-Pfsv9oYmA')
 
-@client.command()
-async def sls11(ctx):
+
+@client.command(name="SLS11")
+async def _sls11(ctx):
     await ctx.send('SLS 11: https://youtu.be/2bWAwH5tXyM')
 
-@client.command()
-async def sls12(ctx):
+
+@client.command(name="SLS12")
+async def _sls12(ctx):
     await ctx.send('SLS 12: https://youtu.be/e96HEzZGjrI')
 
-@client.command()
-async def sls13(ctx):
+
+@client.command(name="SLS13")
+async def _sls13(ctx):
     await ctx.send('SLS 13: https://youtu.be/lGPHTC-jdU8')
 
 
