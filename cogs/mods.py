f5857520d54d2342745048614681a56986834f40
M1zushi <usersama2006@gmail.com>
2021-07-22 17:36:04 +0400

    🔧 Started search result parsing

--- cogs/mods.py
+++ cogs/mods.py
@@ -2,7 +2,12 @@ import discord
 from discord.ext import commands
 
 import asyncio
-import random
+import requests
+import urllib
+
+# import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
 
 import os
 import sys
@@ -25,6 +30,52 @@ class Mods(commands.Cog):
     #     except # Results not found:
     #         await ctx.send('``! Please input a valid mod name !``')
 
+    # def get_source(url):
+    # """Return the source code for the provided URL.
+    #
+    # Args:
+    #     url (string): URL of the page to scrape.
+    #
+    # Returns:
+    #     response (object): HTTP response object from requests_html.
+    # """
+    #
+    # try:
+    #     session = HTMLSession()
+    #     response = session.get(url)
+    #     return response
+    #
+    # except requests.exceptions.RequestException as e:
+    #     print(e)
+    #
+
+    @commands.command(name="Mod Search Command", aliases=['mod'])
+    async def _modsearch(self, ctx, mod):
+
+        def source(url):
+
+            try:
+                session = HTMLSession()
+                response = session.get(url)
+                return response
+
+            except requests.exceptions.RequestException as e:
+                print(e)
+
+        def modsearch(query):
+
+            query = urllib.parse.quote_plus(query)
+            response = source("https://penmodding.pm/?s=" + query)
+            links = list(response.html.absolute_links)
+            penmodding_domain = ('https://penmodding.')
+
+            for url in links[:]:
+                if not url.startswith(penmodding_domain):
+                    links.remove(url)
+
+        modlink = modsearch(mod)
+
+        await ctx.send(f'{modlink}')
 
 
 def setup(client):
